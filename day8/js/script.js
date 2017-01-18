6a3a5ea35a8ae709f309a60023e86353bb88c70f
Konstantinos Vassos <k.vassos@outlook.com>
2017-01-18 12:03:11 +0100

    javascript

---
+++ day8/js/script.js
@@ -0,0 +1,37 @@
+var value =null;
+var type =null;
+
+
+/*value='123' //we assign '123' into variable value
+type = typeof value;  // we get the type of variable 
+console.log(type);
+
+value='123 Times Square' //we assign '123' into variable value
+type = typeof value;  // we get the type of variable 
+console.log(type);
+
+value=123 //we assign '123' into variable value
+type = typeof value;  // we get the type of variable 
+console.log(type);
+
+value=true //we assign '123' into variable value
+type = typeof value;  // we get the type of variable 
+console.log(type);
+
+value='true' //we assign '123' into variable value
+type = typeof value;  // we get the type of variable 
+console.log(type);
+/*123
+'123 Times Square'
+12.3
+true
+'true'
+[1, 2, 3]
+{ foo: 'bar' }
+document
+
+console.log(type);*/
+
+var message_element = document.getElementById('message');
+console.log(message_element);
+console.log(typeof message_element);
